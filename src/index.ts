141b858cb823e954f9ccca068c5740597faed141
Roman Matsypula <ukrmatsypula@gmail.com>
2023-09-21 22:34:53 +0300

    simple render sort in console

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello all");
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([-5, 1, 2, 15, 0, -1, -20]);
+sorter.sort();
+console.log(sorter);
