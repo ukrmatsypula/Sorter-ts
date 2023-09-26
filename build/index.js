41fc4dcc74cc6d30957e11e19ba46c02ded4d857
Roman Matsypula <ukrmatsypula@gmail.com>
2023-09-26 16:12:08 +0300

    add class characters collection

--- build/index.js
+++ build/index.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
 const numberCollection = new NumbersCollection_1.NumbersCollection([-5, 1, 2, 15, 0, -1, -20]);
 const sorter = new Sorter_1.Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection);
+const charactersCollection = new CharactersCollection_1.CharactersCollection("Roman");
+const sorter2 = new Sorter_1.Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numberCollection = new NumbersCollection([-5, 1, 2, 15, 0, -1, -20]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection);
+
+const charactersCollection = new CharactersCollection("Roman");
+
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
+
