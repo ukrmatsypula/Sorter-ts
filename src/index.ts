a5312d5db218139d0baca7795b4fc34595253dee
Roman Matsypula <ukrmatsypula@gmail.com>
2023-09-26 20:51:12 +0300

    add Linded List

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numberCollection = new NumbersCollection([-5, 1, 2, 15, 0, -1, -20]);
 const sorter = new Sorter(numberCollection);
@@ -13,3 +14,12 @@ const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(charactersCollection.data);
 
+const linkedList = new LinkedList();
+linkedList.add(100);
+linkedList.add(-11);
+linkedList.add(-3);
+linkedList.add(7);
+
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
